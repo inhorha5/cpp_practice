0ef14c616d96eae9e2da1aacaeced8ab1cfc6e50
InHo Rha <edwardrha@users.noreply.github.com>
2023-02-04 02:40:33 +0900

    Initial Commit

---
+++ cpp_test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_test
+//
+//  Created by InHo Rha on 2/4/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
