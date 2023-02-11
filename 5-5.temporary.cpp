4594753bc6c36d1f1d010a75c158b9560000d175
edwardrha <edwardrha@users.noreply.github.com>
2023-02-11 15:10:46 +0900

    5-5 added.

---
+++ 5-5.temporary.cpp
@@ -0,0 +1,41 @@
+//
+//  5-5.temporary.cpp
+//  cpp_test
+//
+//  Created by InHo Rha on 2/11/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Temporary {
+private:
+    int num;
+    
+public:
+    Temporary(int n) : num(n) {
+        cout << "Create obj: " << this->num << endl;
+    }
+    
+    ~Temporary() {
+        cout << "destroy obj: " << this->num << endl;
+    }
+    
+    void showTemporaryInfo() {
+        cout << "My num is " << this->num << endl;
+    }
+};
+
+int main() {
+    Temporary(100);
+    cout << "********************after make!" << endl << endl;
+    
+    Temporary(200).showTemporaryInfo();
+    cout << "********************after make!" << endl << endl;
+    
+    const Temporary& reference = Temporary(300);
+    cout << "********************end of the main!" << endl << endl;
+    
+    return 0;
+}
