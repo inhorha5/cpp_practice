11e46ae166075c842d6c9c7b9fe203409e96f78b
edwardrha <edwardrha@users.noreply.github.com>
2023-02-11 16:30:19 +0900

    6-4 commit.

---
+++ cpp_test/6-4.public_static_members.cpp
@@ -0,0 +1,33 @@
+//
+//  6-4.public_static_members.cpp
+//  cpp_test
+//
+//  Created by InHo Rha on 2/11/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class SoSimple2 {
+public:
+    static int simpleObjectCount;
+    
+public:
+    SoSimple2() {
+        this->simpleObjectCount++;
+    }
+};
+int SoSimple2::simpleObjectCount = 0;
+
+int main() {
+    cout << SoSimple2::simpleObjectCount << " SoSimple exists" << endl;
+    SoSimple2 simple1;
+    SoSimple2 simple2;
+    
+    cout << SoSimple2::simpleObjectCount << " SoSimples exist" << endl;
+    cout << simple1.simpleObjectCount << " SoSimples exist" << endl;
+    cout << simple2.simpleObjectCount << " SoSimples exist" << endl;
+    
+    return 0;
+}
